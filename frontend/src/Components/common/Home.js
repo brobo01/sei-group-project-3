71328c76756b21f404743aecd159800a6c333b36
Ben Roberts <benroberts67@hotmail.co.uk>
2020-05-15 15:15:18 +0100

    added front end home, index and show trip pages

---
+++ frontend/src/Components/common/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Home = () => {
+  return (
+    <main>
+            <h1> RoadTrippin </h1>
+            <Link to='/trips'>ENTER</Link>
+    </main>
+
+  )
+}
+
+
+export default Home
---
+++ frontend/src/Components/trips/tripShow.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import { Link } from 'react-router-dom'
+
+
+const tripShow = () => {
+  return (
+    <main>
+            <h1> This is an awesome road trip </h1>
+    </main>
+
+  )
+}
+
+
+export default tripShow

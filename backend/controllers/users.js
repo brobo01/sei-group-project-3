8a6faa78bcb386b5335d17a207771a8bfbda5875
Krissy Little <kristenlittle1992@gmail.com>
2020-05-15 17:58:18 +0100

    fixed issue with register, added to user controller

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,6 +2,27 @@ const User = require('../models/user')
 const { notFound } = require('../lib/errorMessages')
 
 async function userProfile(req, res, next) {
+  const userId = req.params.userId
+  try {
+    const user = await User.findById(userId)
+    if (!user) throw new Error(notFound)
+    res.status(200).json(user)
+  } catch (err) {
+    next(err)
+  }
+}
+
+async function userIndex(req, res, next) {
+  try {
+    const users = await User.find()
+    if (!users) throw new Error(notFound)
+    res.status(200).json(users)
+  } catch (err) {
+    next(err)
+  }
+}
+
+async function indivProfile(req, res, next) {
   try {
     const user = await User.findById(req.currentUser._id)
     if (!user) throw new Error(notFound)
@@ -11,19 +32,22 @@ async function userProfile(req, res, next) {
   }
 }
 
-
-async function userProfileEdit(req, res, next) {
+async function indivProfileEdit(req, res, next) {
   try {
     const user = await User.findById(req.currentUser._id)
     if (!user) throw new Error(notFound)
-    Object.assign(trip, req.body)
-    await trip.save()
-    res.status(202).json(trip)
+    Object.assign(user, req.body)
+    await user.save()
+    res.status(202).json(user)
   } catch (err) {
     next(err)
   }
 }
 
 module.exports = {
-  profile: userProfile
+  userProfile: userProfile,
+  userIndex: userIndex,
+  showProfile: indivProfile,
+  editProfile: indivProfileEdit
+
 }
--- backend/db/data/users.js
+++ backend/db/data/users.js
@@ -4,22 +4,25 @@ module.exports = [
     name: 'Ben Roberts',
     email: 'ben@email',
     password: 'pass',
+    passwordConfirmation: 'pass',
     profilePhoto: null,
     garage: 'Harley Davidson',
     dreamTrips: 'Winner of Le Mans race'
-  },{
+  }, {
     username: 'KrissyKittenKatcher',
     name: 'Krissy Little',
     email: 'krissy@email',
     password: 'pass',
+    passwordConfirmation: 'pass',
     profilePhoto: null,
     garage: 'Classic Cadillac',
     dreamTrips: 'Car trip around Morroco'
-  },{
+  }, {
     username: 'A-Der',
     name: 'Assia Deramchi',
     email: 'assia@email',
     password: 'pass',
+    passwordConfirmation: 'pass',
     profilePhoto: null,
     garage: 'Yamaha XT225 Serow',
     dreamTrips: 'North to South of the Americas'

378ca5c2b00f8946a48192a022ea0ea5d7e51954
Ben Roberts <benroberts67@hotmail.co.uk>
2020-05-15 11:58:54 +0100

    added in index page, error hadnling, environment page and basic route for tripIndex page

---
+++ backend/config/environment.js
@@ -0,0 +1,10 @@
+const dbURI = 'mongodb://localhost/sei-group-project-3-db'
+const port = 8000
+const secret = 'its a secret'
+
+
+module.exports = {
+  dbURI,
+  port,
+  secret
+}
---
+++ backend/config/routes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router()
+const trips = require('../controllers/trips')
+// const auth = require('../controllers/auth')
+// const secureRoute = require('../lib/secureRoute')
+// const user = require('../controllers/users')
+
+
+router.route('/trips')
+  .get(trips.index)
+  // .post(trips.create)
+  // .post(secureRoute, trips.create)
+  
+// router.route('/trips/:id')
+//   .get(trips.show)
+// .put(trips.update)
+// .delete(trips.delete)
+// .put(secureRoute, trips.update)
+// .delete(secureRoute, trips.delete)
+
+// router.route('/trips/:id/comments')
+//   .post(trips.commentCreate)
+//   .post(secureRoute, trips.commentCreate)
+
+// router.route('/trips/:id/comments/:commentId')
+//   .delete(trips.commentDelete)
+//   .delete(secureRoute, trips.commentDelete)
+
+// router.route('/profile')
+//   .get(user.profile)
+//   .get(secureRoute, user.profile)
+
+// router.route('/register')
+//   .post(auth.register)
+
+// router.route('/login')
+//   .post(auth.login)
+
+module.exports = router

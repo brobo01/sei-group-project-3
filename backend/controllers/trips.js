719eaa0ebc05ab29e370105019b2c569959c59c2
Krissy Little <kristenlittle1992@gmail.com>
2020-05-15 16:52:17 +0100

    trips controllers finished

--- backend/controllers/trips.js
+++ backend/controllers/trips.js
@@ -14,18 +14,20 @@ async function tripsIndex(req, res, next) {
 
 // create
 
-async function tripsCreate(req, res) {
+async function tripsCreate(req, res, next) {
   try {
+    //req.body.user = req.currentUser 
     const newTrip = await Trips.create(req.body)
-    res.status(202).json(newTrip)
+
+    res.status(201).json(newTrip)
   } catch (err) {
-    res.json(err)
+    next(err)
   }
 }
 
 // read
 
-async function tripsShow(req, res) {
+async function tripsShow(req, res, next) {
   const tripId = req.params.id
   try {
     const trip = await Trips.findById(tripId)
@@ -33,38 +35,39 @@ async function tripsShow(req, res) {
     res.status(200).json(trip)
 
   } catch (err) {
-    res.status(404).json({ 'message': 'Not found' })
+    next(err)
   }
 
 }
 
 // update
 
-async function tripsUpdate(req, res) {
+async function tripsUpdate(req, res, next) {
   const tripId = req.params.id
   try {
-    const trips = await Trips.findByIdAndUpdate(
-      tripId,
-      req.body,
-      { new: true, runValidators: true }
-    )
-    res.status(202).json(trips)
+    const trip = await Trips.findById(tripId)
+    if (!trip) throw new Error(notFound)
+    // if (!trip.user.equals(req.currentUser._id)) throw new Error()
+    Object.assign(trip, req.body)
+    await trip.save()
+    res.status(202).json(trip)
   } catch (err) {
-    res.json(err)
+    next(err)
   }
 }
 
 // delete
 
-async function tripsDelete(req, res) {
+async function tripsDelete(req, res, next) {
   const tripId = req.params.id
   try {
     const tripToDelete = await Trips.findById(tripId)
     if (!tripToDelete) throw new Error('Not Found')
+    // if (!trip.user.equals(req.currentUser._id)) throw new Error()
     await tripToDelete.remove()
     res.sendStatus(204)
   } catch (err) {
-    res.json(err)
+    next(err)
   }
 }
 
@@ -72,20 +75,41 @@ async function tripsDelete(req, res) {
 
 async function recommendationsCreate(req, res, next) {
   try {
+    //req.body.user = req.currentUser
     const tripId = req.params.id
     const trip = await Trips.findById(tripId)
     if (!trip) throw new Error(notFound)
-
+    trip.recommendations.push(req.body)
+    await trip.save()
+    res.status(201).json(trip)
   } catch (err) {
     next(err)
   }
 }
 
+async function recommendationsDelete(req, res, next) {
+  try {
+    const tripId = req.params.id
+    const commentId = req.params.commentId
+    const trip = await Trips.findById(tripId)
+    if (!trip) throw new Error(notFound)
+    const commentToRemove = trip.recommendations.id(commentId)
+    if (!commentToRemove) throw new Error(notFound)
+    // if (!commentToRemove.user.equals(req.currentUser._id) || !trip.user.equals(req.currentUser._id)) throw new Error()
+    await commentToRemove.remove()
+    await trip.save()
+    res.sendStatus(204)
+  } catch (err) {
+    next(err)
+  }
+}
 
 module.exports = {
   index: tripsIndex,
   create: tripsCreate,
   show: tripsShow,
   update: tripsUpdate,
-  delete: tripsDelete
+  delete: tripsDelete,
+  commentCreate: recommendationsCreate,
+  commentDelete: recommendationsDelete
 }
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -11,6 +11,19 @@ async function userProfile(req, res, next) {
   }
 }
 
+
+async function userProfileEdit(req, res, next) {
+  try {
+    const user = await User.findById(req.currentUser._id)
+    if (!user) throw new Error(notFound)
+    Object.assign(trip, req.body)
+    await trip.save()
+    res.status(202).json(trip)
+  } catch (err) {
+    next(err)
+  }
+}
+
 module.exports = {
   profile: userProfile
 }
